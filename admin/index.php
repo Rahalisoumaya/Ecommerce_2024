1bd6786432343230946b8798dab88cdb8e77cc68
rahali soumaya <rahalisou@gmail.com>
2024-11-11 13:47:58 +0100

    version1:background projet ecommerce

---
+++ admin/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" >
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    include "nav.php";
+    ?>
+    <div class="container mt-3">
+
+    
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quisquam, modi fugit reprehenderit ab delectus ex at voluptatum vero neque, natus impedit commodi repellendus doloremque alias totam repellat et incidunt.
+    Provident reiciendis totam animi amet soluta fugit odit corporis saepe voluptatibus cumque in nam nisi molestiae dolore odio minima pariatur a consequatur temporibus dicta architecto eligendi non, molestias dolorem! Sed.
+    Ab exercitationem illo debitis, velit quidem aliquid temporibus consequuntur facere officiis similique a odio delectus eligendi hic! Ducimus molestias, beatae, molestiae sint aliquam numquam eius velit et, fuga enim maxime.
+    Quia minima, perferendis sequi reiciendis molestiae ducimus nemo facilis. Est perferendis iure, aperiam error repellendus sunt fugiat natus tenetur esse incidunt magni exercitationem, doloribus facilis distinctio, ex voluptates iste fugit!
+    Assumenda maxime temporibus voluptatibus perferendis, soluta quis similique beatae, odit rem harum accusantium enim rerum. Consequatur, aut aspernatur pariatur ad, numquam ipsa fugiat delectus deserunt quisquam at sequi beatae error.
+    Saepe ipsum nemo optio eius ut libero natus at praesentium consectetur reprehenderit nulla ducimus autem nihil ea corporis quod eum assumenda id repudiandae distinctio, sequi eos incidunt. Adipisci, officiis sequi.
+    Odit earum nostrum repellendus. Exercitationem sapiente officia impedit sunt quibusdam accusantium, commodi quasi magni id sed tenetur veritatis corrupti magnam ea ab illum atque doloribus at quae quaerat voluptatem delectus?
+    Optio neque illo tenetur vel sapiente vitae earum? Facilis, impedit atque. Incidunt officiis accusamus explicabo porro repudiandae quia earum nisi expedita, ipsa vero rem enim odit quod optio, sit possimus.
+    Dignissimos voluptatibus cupiditate amet at vero deleniti aliquid repudiandae debitis ex. Tenetur, velit qui veritatis asperiores facere praesentium aperiam veniam odit? Ad saepe aperiam alias exercitationem nobis laborum maxime necessitatibus?
+    Quia doloribus natus sunt porro, libero quidem, quos inventore praesentium, voluptatem nulla magni quisquam commodi ullam! Voluptates vel alias ex sequi culpa cumque, ullam perspiciatis dolores molestiae quo, velit pariatur.</h1>
+    </div>
+</body>
+</html>
